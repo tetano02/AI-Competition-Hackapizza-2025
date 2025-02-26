525b601bdf29e68d8835dfec75af8e4776e95006
Tetano <s.agnelli004@studenti.unibs.it>
2025-02-26 21:57:43 +0100

    Gemini API

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("LOL")
+# Open the file apikey.txt and read the API key from it
+with open("Secret Data/apikey.txt", "r") as file:
+    API_KEY = file.read().strip()
+
+from google import genai
+
+client = genai.Client(api_key=API_KEY)
+
+question = "Ciao, dimmi qualcosa di interessante"
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=question,
+)
+
+print("-----------------GEMINI MODEL 2.0 FLASH----------------\n")
+print("-----------------Question------------------------------\n")
+print(question+"\n")
+print("-----------------Response------------------------------\n")
+print(response.text)
